e191668360263d3bd8e694b99bf639b87c974e35
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-08-21 03:33:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 # Stop the bots
 echo "Stopping bots..."
-python3 -c "from Oristella import bot, app; await bot.stop(); await app.stop()"
+python3 -c "import asyncio; from Oristella import bot, app; async def main(): await bot.stop(); await app.stop(); asyncio.run(main())"
 
 # Close the aiohttp client session to avoid "Unclosed client session" error
 echo "Closing aiohttp client session..."
-python3 -c "from aiohttp import ClientSession; aiohttpsession = ClientSession(); aiohttpsession.close()"
+python3 -c "import asyncio; from aiohttp import ClientSession; async def main(): async with ClientSession() as aiohttpsession: pass; asyncio.run(main())"
 
 echo "Deployment complete"
