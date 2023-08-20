16f87ca0adc77561979bc2bd72159385768883e4
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-08-20 23:36:13 +0530

    Update start.py

--- Oristella/utils/start.py
+++ Oristella/utils/start.py
@@ -23,11 +23,11 @@ async def get_learn(_, m: Message, help_option: str):
 the other way to use me is to write the inline query by your self
 the format should be in this arrangement
 
-`@TheOristellabot your whisper @username`
+`@dOristellaBot your whisper @username`
 
 now I'll split out the format in 3 parts and explain every part of it
 
-1- `@TheOristellabot`
+1- `@dOristellaBot`
 this is my username it should be at the beginning of the inline query so I'll know that you are using me and not another bot.
 
 2- `whisper message`
@@ -37,7 +37,7 @@ it is the whisper that will be sent to the target user, you need to remove your 
 you should replace this with target's username so the bot will know that the user with this username can see your whisper message.
 
 example:- 
-`@TheOristellabot hello this is a test @maxsupun`
+`@dOristellaBot hello this is a test @NickyLrca`
 
 📎 the bot works in groups and the target user should be in the same group with you
 what you are waiting for?!
